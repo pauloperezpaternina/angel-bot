cd272a897daaea95485e424836004e959892d3d7
Paulo Pérez <pauloperez@bittronic.net>
2024-08-07 22:47:30 -0500

    initial chatbot configuration

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# angel-bot
+
+Chatbot to reserve cabins in Tierrabomba using Google Calendar.
---
+++ app.js
@@ -0,0 +1,26 @@
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+
+
+const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
+    .addAnswer('🙌 Hola bienvenido(a) a *Angelbot*')
+
+
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterFlow = createFlow([flowPrincipal])
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
